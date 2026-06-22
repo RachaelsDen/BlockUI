fa4919ec52d4271f3de9095b5a28fe4347317e35
Katelyn Goodwin <datateccs@gmail.com>
2026-06-22 00:25:04 -0700

    fabric: fix defer client init to CLIENT_STARTED to prevent NPE (port-divergent)
    ClientLifecycleSubscriber.register() and ClientEventSubscriber.register()
    both call Minecraft.getInstance().getResourceManager() which returns null
    during onInitializeClient(). Deferring to ClientLifecycleEvents.CLIENT_STARTED
    ensures Minecraft is fully initialized before reload listeners are registered.

--- src/main/java/com/ldtteam/blockui/mod/BlockUIClient.java
+++ src/main/java/com/ldtteam/blockui/mod/BlockUIClient.java
@@ -1,13 +1,16 @@
 package com.ldtteam.blockui.mod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 public class BlockUIClient implements ClientModInitializer
 {
     @Override
     public void onInitializeClient()
     {
-        ClientLifecycleSubscriber.register();
-        ClientEventSubscriber.register();
+        ClientLifecycleEvents.CLIENT_STARTED.register(minecraft -> {
+            ClientLifecycleSubscriber.register();
+            ClientEventSubscriber.register();
+        });
     }
 }
